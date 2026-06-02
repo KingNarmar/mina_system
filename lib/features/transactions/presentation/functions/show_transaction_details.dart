b044ad38f86a5a163e5fd870ab58335f3b9e2c5a
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-02 15:28:23 +0400

    Provide transactions cubit to transaction details dialog

--- lib/features/transactions/presentation/functions/show_transaction_details.dart
+++ lib/features/transactions/presentation/functions/show_transaction_details.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mina_system/features/current_context/presentation/cubit/current_context_cubit.dart';
 import 'package:mina_system/features/transactions/data/models/transaction_model.dart';
+import 'package:mina_system/features/transactions/presentation/cubit/transactions_cubit.dart';
 import 'package:mina_system/features/transactions/presentation/widgets/details/transaction_details_dialog.dart';
 
 void showTransactionDetails(
@@ -9,12 +10,16 @@ void showTransactionDetails(
   TransactionModel transaction,
 ) {
   final currentContextCubit = context.read<CurrentContextCubit>();
+  final transactionsCubit = context.read<TransactionsCubit>();
 
   showDialog(
     context: context,
     builder: (_) {
-      return BlocProvider.value(
-        value: currentContextCubit,
+      return MultiBlocProvider(
+        providers: [
+          BlocProvider.value(value: currentContextCubit),
+          BlocProvider.value(value: transactionsCubit),
+        ],
         child: Dialog(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(20),
