030f1da5ae5aabd014625b75f413922e887d2327
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-02 15:27:09 +0400

    Show voided status in transaction details header

--- lib/features/transactions/presentation/widgets/details/transaction_details_header.dart
+++ lib/features/transactions/presentation/widgets/details/transaction_details_header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:mina_system/core/theme/app_colors.dart';
 import 'package:mina_system/core/theme/app_text_styles.dart';
 import 'package:mina_system/features/transactions/data/models/transaction_model.dart';
 import 'package:mina_system/features/transactions/presentation/functions/transaction_type_helpers.dart';
@@ -27,21 +28,43 @@ class TransactionDetailsHeader extends StatelessWidget {
         Expanded(
           child: Text(transaction.transactionCode, style: AppTextStyles.title),
         ),
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-          decoration: BoxDecoration(
-            color: typeColor.withValues(alpha: 0.12),
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Text(
-            typeLabel,
-            style: AppTextStyles.caption.copyWith(
-              color: typeColor,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
+        _StatusBadge(
+          label: typeLabel,
+          color: typeColor,
         ),
+        if (transaction.isVoided) ...[
+          const Gap(8),
+          const _StatusBadge(
+            label: 'Voided',
+            color: AppColors.error,
+          ),
+        ],
       ],
+    );
+  }
+}
+
+class _StatusBadge extends StatelessWidget {
+  const _StatusBadge({required this.label, required this.color});
+
+  final String label;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+      decoration: BoxDecoration(
+        color: color.withValues(alpha: 0.12),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Text(
+        label,
+        style: AppTextStyles.caption.copyWith(
+          color: color,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
     );
   }
 }
